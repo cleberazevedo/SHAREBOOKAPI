b922cb03a49fc98b85af89eb0b284dbd6ec5bcb3
clebermago <cleber.azevedo@finanzero.com.br>
2023-03-26 20:30:03 -0300

    Add project files.

---
+++ SHAREBOOK.API/Controllers/SharebookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SHAREBOOK.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SharebookController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+       {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public SharebookController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "SharebookController")]
+        public IEnumerable<WeatherForecast> Post()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
